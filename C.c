7cc852aeca34a0e5675260576a41bd4eb2ae5d88
sjvbqwinvorusbiewaj <antonio.proscia@linussio.it>
2025-05-28 20:02:00 +0200

    Add files via upload

---
+++ C.c
@@ -0,0 +1,13 @@
+int main(void)  {
+    int x = 5;
+    int y = 7; 
+    if (x>=y){
+        printf(x);
+    } else {
+        printf("%d%, x); 
+    }
+    
+}
+
+
+
---
+++ C.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    printf("Hello world!\n");
+    int A = 18; B = 208
+    printf("%d", A+B)
+    return 0;
+}
+
+/*
+//
+int main(void) {
+    int a=207, b=3;
+    printf("%d", a/b);
+    return 0;
+}
+
+int main(void) {
+    int x = 5;
+    int y = 7;
+    if (x>=y){
+        printf("%d", x);
+    } else {
+        printf("%d), y);
+    } else {
+        printf("%d", z);
+    }
+}
+
